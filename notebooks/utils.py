2f6c872a8f3b7de4fded962659657de2313660bb
Kim Leirvik <kim.leirvik@niva.no>
2026-02-17 19:26:39 +0100

    setup for next run and slippting to dedicated features method

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -27,15 +27,20 @@ MARINE_VANN_TYPE_DESC = {
 
 VANNTYPER_COMBINED = {
     "beskyttet": ["01", "01a", "02", "02a", "03", "03a", "09"],
-    #"sterkt_ferskvannspåvirket": [, ], moved to beskyttet
+    # "sterkt_ferskvannspåvirket": [, ], moved to beskyttet
     "moderat_eksponert": ["04", "05", "08"],
     "bølgeeksponert": ["06", "07"],
-    #"strømrike": ["08"], moved to moderat_eksponert
-    #"særegen": ["09"], moved to beskyttet
+    # "strømrike": ["08"], moved to moderat_eksponert
+    # "særegen": ["09"], moved to beskyttet
 }
 
+BUNNTYPE_MAPPING = {"løsbunn": 0, "blanding/uspesifisert": 1, "fastbunn": 2}
+
+
 def one_hot_encode_marine_types(marine_type_raster, id_type_map, combine_types=False):
-    num_classes = len(MARINE_VANN_TYPE_DESC) if not combine_types else len(VANNTYPER_COMBINED)
+    num_classes = (
+        len(MARINE_VANN_TYPE_DESC) if not combine_types else len(VANNTYPER_COMBINED)
+    )
 
     one_hot_types = np.zeros(marine_type_raster.shape + (num_classes,), dtype=np.uint8)
 
@@ -48,13 +53,21 @@ def one_hot_encode_marine_types(marine_type_raster, id_type_map, combine_types=F
 
 
 def rasterize_marine_types(mv_proj, dem, combine_types=False):
-    types = MARINE_VANN_TYPE_DESC.keys() if not combine_types else VANNTYPER_COMBINED.keys()
+    types = (
+        MARINE_VANN_TYPE_DESC.keys() if not combine_types else VANNTYPER_COMBINED.keys()
+    )
     type_id_map = {t: i for i, t in enumerate(types)}
     id_type_map = {i: t for t, i in type_id_map.items()}
 
     # Prepare shapes as (geometry, value)
     mv_proj = mv_proj.copy()
-    mv_proj["type_key"] = mv_proj["Type"] if not combine_types else mv_proj["Type"].map(lambda x: next((k for k, v in VANNTYPER_COMBINED.items() if x in v), x))
+    mv_proj["type_key"] = (
+        mv_proj["Type"]
+        if not combine_types
+        else mv_proj["Type"].map(
+            lambda x: next((k for k, v in VANNTYPER_COMBINED.items() if x in v), x)
+        )
+    )
     shapes = [
         (geom, type_id_map[t]) for geom, t in zip(mv_proj.geometry, mv_proj["type_key"])
     ]
@@ -101,9 +114,11 @@ def plot_one_hot_vanntyper(one_hot_types, id_type_map, transform, crs, cols, row
 def plot_vanntyper(marine_type_raster, transform, id_type_map):
     masked = np.ma.masked_less(marine_type_raster, 0)
 
-    description = MARINE_VANN_TYPE_DESC if "01" in id_type_map else {
-        k: " ".join(VANNTYPER_COMBINED[k]) for k in id_type_map.values()
-    }
+    description = (
+        MARINE_VANN_TYPE_DESC
+        if "01" in id_type_map
+        else {k: " ".join(VANNTYPER_COMBINED[k]) for k in id_type_map.values()}
+    )
     # Discrete colormap and norm
     num_classes = len(id_type_map)
     cmap = plt.cm.get_cmap("tab20", num_classes)
@@ -117,8 +132,7 @@ def plot_vanntyper(marine_type_raster, transform, id_type_map):
 
     tick_locs = np.arange(num_classes)
     tick_labels = [
-        f"{i}: {id_type_map[i]} – {description[id_type_map[i]]}"
-        for i in tick_locs
+        f"{i}: {id_type_map[i]} – {description[id_type_map[i]]}" for i in tick_locs
     ]
     cbar = fig.colorbar(im, ax=ax, ticks=tick_locs, fraction=0.046, pad=0.04)
     cbar.ax.set_yticklabels(tick_labels)
@@ -136,7 +150,9 @@ def to_postgis(gdf, fname):
     if os.environ.get("NIVAGIS_CONNECTION_STR"):
         table_name = "_".join(fname.split("_")[:-1])[:50]
         conn = create_engine(os.environ["NIVAGIS_CONNECTION_STR"])
-        gdf.to_postgis(table_name, schema="naturkartmarin", con=conn, if_exists="replace")
+        gdf.to_postgis(
+            table_name, schema="naturkartmarin", con=conn, if_exists="replace"
+        )
         print(f"Table {table_name} uploaded to PostGIS.")
     else:
         print("NIVAGIS_CONNECTION_STR not set. Skipping PostGIS upload.")
