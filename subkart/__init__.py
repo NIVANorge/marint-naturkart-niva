5137e187cba2ad878ca98130bebc1bce003502d3
Kim Leirvik <kim.leirvik@niva.no>
2026-05-12 12:33:30 +0200

    prerun

--- subkart/__init__.py
+++ subkart/__init__.py
@@ -1,3 +1,7 @@
+import rasterio as rio
+
 from subkart import features, labelling, plot, sources, utils, training
 
 __all__ = ["features", "utils", "labelling", "sources", "plot", "training"]
+
+rio.Env(GS_NO_SIGN_REQUEST="YES").__enter__()
