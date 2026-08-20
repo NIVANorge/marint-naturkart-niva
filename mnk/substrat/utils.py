db6a4bdb348508d49f9d2f23cbb17602f52ea548
Kim Leirvik <kim.leirvik@niva.no>
2026-08-20 13:50:34 +0200

    refactor imports

--- mnk/substrat/utils.py
+++ mnk/substrat/utils.py
@@ -9,7 +9,6 @@ import xdem
 from osgeo import gdal
 from shapely.errors import GEOSException
 from shapely.validation import explain_validity, make_valid
-from sqlalchemy import create_engine
 
 import mnk.substrat as subkart
 
@@ -301,27 +300,6 @@ def to_geoparquet(input_gml_path: Path, layer_name: str, output_path: Path):
     print(f"Written GeoParquet to {output_path}")
 
 
-def to_filename(ressurstittel, romligutstrekning, ressursdato, referansesystem):
-
-    return f"{ressurstittel}_{romligutstrekning}_{ressursdato}_{referansesystem}"
-
-
-def to_postgis(gdf, fname):
-
-    if os.environ.get("NIVAGIS_CONNECTION_STR"):
-        table_name = to_tablename(fname)
-        conn = create_engine(os.environ["NIVAGIS_CONNECTION_STR"])
-        gdf.to_postgis(table_name, schema="naturkartmarin", con=conn, if_exists="replace")
-        print(f"Table {table_name} uploaded to PostGIS.")
-    else:
-        print("NIVAGIS_CONNECTION_STR not set. Skipping PostGIS upload.")
-
-
-def to_tablename(fname: str) -> str:
-    """Convert a filename to a table name."""
-    return "_".join(fname.lower().split("_")[0:3]).replace("-", "_")[:50]
-
-
 def save_feature_rasters(
     out_dir: Path,
     prefix: str,
@@ -352,42 +330,3 @@ def save_feature_rasters(
             nodata=nodata,
         ) as dst:
             dst.write(band, 1)
-
-
-def parquet_to_postgis(parquet_path: str, crs: str = "EPSG:25833"):
-    """Stream a GeoParquet file from GCS to PostGIS using GDAL."""
-
-    gdal.UseExceptions()
-
-    if not os.environ.get("NIVAGIS_CONNECTION_STR"):
-        print("NIVAGIS_CONNECTION_STR not set. Skipping PostGIS upload.")
-        return
-
-    pg_conn_str = os.environ["NIVAGIS_CONNECTION_STR"]
-
-    print("Streaming GeoParquet from GCS to PostGIS using osgeo.gdal...")
-
-    source = f"/vsicurl/{parquet_path}" if parquet_path.startswith("https://") else parquet_path
-    table_name = "naturkartmarin." + to_tablename(os.path.basename(parquet_path).split(".")[0])
-    gdal.SetConfigOption("PG_USE_COPY", "YES")
-
-    options = gdal.VectorTranslateOptions(
-        format="PostgreSQL",
-        layerName=table_name,
-        layerCreationOptions=[
-            "GEOMETRY_NAME=geom",
-            "FID=fid",
-            "SPATIAL_INDEX=GIST",
-        ],
-        callback=gdal.TermProgress_nocb,
-        SRC_SRS=crs,
-        DST_SRS=crs,
-    )
-
-    gdal.VectorTranslate(
-        destNameOrDestDS=f"PG:{pg_conn_str}",
-        srcDS=source,
-        options=options,
-    )
-
-    print("Table successfully loaded!")
